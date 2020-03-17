fe61840c02718a0d2910e9c7619f9668e9cb06be
w1138281007 <863327166@qq.com>
2020-03-17 10:45:50 +0800

    修复api失效

--- src/api/movies.js
+++ src/api/movies.js
@@ -5,7 +5,7 @@ export function GetTheaters () {
     key: ';superagentCallback1539693291707915'
   }
   return new Promise((resolve, reject) => {
-    let url = 'https://api.douban.com/v2/movie/in_theaters?count=8'
+    let url = 'https://api.douban.com/v2/movie/in_theaters?count=8&apikey=0df993c66c0c636e29ecbb5344252a4a'
     jsonp(url, options, (err, data) => {
       if (err) {
         reject(err)
@@ -18,7 +18,7 @@ export function GetTop () {
   let options = {
     key: ';superagentCallback1540262304444510'
   }
-  let url = 'https://api.douban.com/v2/movie/top250?count=8'
+  let url = 'https://api.douban.com/v2/movie/top250?count=8&apikey=0df993c66c0c636e29ecbb5344252a4a'
   return new Promise((resolve, reject) => {
     jsonp(url, options, (err, data) => {
       if (err) {
@@ -33,7 +33,7 @@ export function GetComingSoon () {
   let options = {
     key: ';superagentCallback1540262304444510'
   }
-  let url = 'https://api.douban.com/v2/movie/coming_soon?count=8'
+  let url = 'https://api.douban.com/v2/movie/coming_soon?count=8&apikey=0df993c66c0c636e29ecbb5344252a4a'
   return new Promise((resolve, reject) => {
     jsonp(url, options, (err, data) => {
       if (err) {
